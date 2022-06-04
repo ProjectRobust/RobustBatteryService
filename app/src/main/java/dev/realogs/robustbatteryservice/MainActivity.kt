00d65323d460a4b82308db1761f18d87d70a1823
Debayan Ghosh Dastider <debz.exe@gmail.com>
2022-06-04 13:42:35 +0530

    Voltage,current and other power sources code conditions

--- app/src/main/java/dev/realogs/robustbatteryservice/MainActivity.kt
+++ app/src/main/java/dev/realogs/robustbatteryservice/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
+import android.view.View
 import dev.realogs.robustbatteryservice.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
@@ -70,6 +71,72 @@ class BatteryStatsService : BroadcastReceiver(){
         if (health == BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE) {
             battery_condition = "Error"
         }
+
+        //Battery Temperature
+        var temperature_celcius: Int =
+            ((intent?.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0))!!) / 10
+        var temperature_far: Double = (temperature_celcius * 1.8 + 32)
+
+        //Battery Charge Power Source
+        var chargePlug = intent?.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)
+        if (chargePlug == BatteryManager.BATTERY_PLUGGED_USB) {
+            power_source = "USB"
+        }
+        if (chargePlug == BatteryManager.BATTERY_PLUGGED_AC) {
+            power_source = "AC Source"
+        }
+        if (chargePlug == BatteryManager.BATTERY_PLUGGED_WIRELESS) {
+            power_source = "Wireless"
+        }
+
+        //Battery Charging Status
+        var status: Int = intent?.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+        if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
+            charging_status = "Charging"
+            binding.batteryCharging.visibility = View.VISIBLE
+            binding.batteryLow.visibility = View.INVISIBLE
+        }
+
+        if (status == BatteryManager.BATTERY_STATUS_DISCHARGING) {
+            charging_status = "Discharging"
+            binding.batteryCharging.visibility = View.INVISIBLE
+            binding.batteryLow.visibility = View.INVISIBLE
+        }
+
+        if (status == BatteryManager.BATTERY_STATUS_FULL) {
+            charging_status = "Full"
+            binding.batteryCharging.visibility = View.INVISIBLE
+            binding.batteryLow.visibility = View.INVISIBLE
+        }
+
+        if (status == BatteryManager.BATTERY_STATUS_UNKNOWN) {
+            charging_status = "Unknown"
+            binding.batteryCharging.visibility = View.INVISIBLE
+            binding.batteryLow.visibility = View.INVISIBLE
+        }
+
+        if (status == BatteryManager.BATTERY_STATUS_NOT_CHARGING) {
+            charging_status = "Unplugged"
+            binding.batteryCharging.visibility = View.INVISIBLE
+            binding.batteryLow.visibility = View.VISIBLE
+        }
+
+        if (level != null) {
+            if (level <= 15) {
+                binding.batteryCharging.visibility = View.INVISIBLE
+                binding.batteryLow.visibility = View.VISIBLE
+            }
+
+        }
+
+        var voltage: Int = intent?.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0)
+        var current =
+            intent?.getIntExtra(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW.toString(), 0)
+
+        var chargeCounter: Int = intent?.getIntExtra(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER.toString(), -1)
+        var capacity: Int = intent?.getIntExtra(BatteryManager.BATTERY_PROPERTY_CAPACITY.toString(),-1)
+
+        var battery: Int = (chargeCounter/capacity) * 100
 
         binding.tvBatteryPercent.setText(level)
         binding.progressBar.setProgress(level, true)
